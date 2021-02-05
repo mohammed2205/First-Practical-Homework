4a35e1be9f4a515aed6e7cd244b13f85fd33168f
MOHAMMED <MOHAMMED@DESKTOP-2H96LKI>
2021-02-05 06:04:28 +0300

    First commit

---
+++ First-Practical-Homework/src/FirstPracticalHomework.java
@@ -0,0 +1,11 @@
+//this program is writtenc by Mohammd hadi
+//Date:Feb 5,2021
+//course Name:212css-4 object oriented progrming 
+public class FirstPracticalHomework {
+
+	public static void main(String[] args) {
+		
+System.out.println("Mohammed hadi");
+	}
+
+}
